c0f7eefceef2440897ccac23bbd498013c1a0dba
Eric Hechavarria <erichech@gmail.com>
2023-11-10 06:11:19 -0500

    try

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,49 @@
 const path = require("path");
 
-const PACKAGE_NAME = "firebase-soil";
-
-const config = {
-  context: __dirname,
+module.exports = {
   entry: {
-    app: "./src/index.ts",
-    server: "./src/server.ts",
+    index: "./src/index.ts",
     client: "./src/client.ts",
+    server: "./src/server.ts",
   },
-  output: {
-    filename: "[name].bundle.js",
-    clean: true,
-    path: path.resolve(__dirname, "dist"),
-    libraryTarget: "umd",
-    library: "[name]",
-    umdNamedDefine: true,
-    globalObject: "this",
-  },
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.ts?$/,
-        loader: "ts-loader",
+        test: /\.tsx?$/,
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
+    fallback: {
+      fs: false,
+      net: false,
+      tls: false,
+      request: false,
+      child_process: false,
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      path: require.resolve("path-browserify"),
+      zlib: require.resolve("browserify-zlib"),
+      os: require.resolve("os-browserify/browser"),
+      buffer: require.resolve("buffer"),
+      url: require.resolve("url"),
+      util: require.resolve("util"),
+      assert: require.resolve("assert"),
+      querystring: require.resolve("querystring-es3"),
+    },
   },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    // See: https://webpack.js.org/configuration/output/#outputlibrary for more information
+    // Also see: https://webpack.js.org/configuration/output/#outputlibrary
+    library: ["global", "client", "server"],
   },
-};
-
-module.exports = (env, argv) => {
-  return config;
 };
