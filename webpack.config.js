b9a173c416f510696c5ffcc7e77250a8cfa8853d
Eric Hechavarria <erichech@gmail.com>
2023-11-10 06:03:01 -0500

    another phase

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,40 @@
 const path = require("path");
 
-module.exports = {
-  // See: https://webpack.js.org/configuration/output/#outputlibrary for more information
-  // Also see: https://webpack.js.org/configuration/output/#outputlibrary
+const PACKAGE_NAME = "firebase-soil";
+
+const config = {
+  context: __dirname,
   entry: {
-    main: {
-      import: "./src/index.ts",
-      library: { name: "global", type: "commonjs" },
-    },
-    client: {
-      import: "./src/client.ts",
-      library: { name: "client", type: "commonjs" },
-    },
-    server: {
-      import: "./src/server.ts",
-      library: { name: "server", type: "commonjs" },
-    },
+    app: "./src/index.ts",
+    server: "./src/server.ts",
+    client: "./src/client.ts",
   },
-  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    clean: true,
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd",
+    library: "[name]",
+    umdNamedDefine: true,
+    globalObject: "this",
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: "ts-loader",
+        test: /\.ts?$/,
+        loader: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    fallback: {
-      fs: false,
-      net: false,
-      tls: false,
-      request: false,
-      child_process: false,
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      path: require.resolve("path-browserify"),
-      zlib: require.resolve("browserify-zlib"),
-      os: require.resolve("os-browserify/browser"),
-      buffer: require.resolve("buffer"),
-      url: require.resolve("url"),
-      util: require.resolve("util"),
-      assert: require.resolve("assert"),
-      querystring: require.resolve("querystring-es3"),
-    },
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
   },
+};
+
+module.exports = (env, argv) => {
+  return config;
 };
