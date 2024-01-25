585f1b2045af36ae963b365433320ddee9397ad7
Eric Hechavarria <erichech@gmail.com>
2024-01-25 05:57:01 -0500

    fix

--- src/services/types.ts
+++ src/services/types.ts
@@ -161,7 +161,7 @@ export type GetOwnersDataParams<T2 extends keyof SoilDatabase> = {
 
 export type ListenerPaginationOptions = { orderBy?: "key" | "value" | { path: string } } & (
   | {
-      exclusiveSide: { direction: "high" | "low"; exclusiveTermination: string | number };
+      exclusiveSide?: { direction: "high" | "low"; exclusiveTermination: string | number };
       exclusiveBetween?: undefined;
       limit?: undefined;
     }
